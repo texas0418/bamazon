42777629d6c81cf84669544ee893700b329e94e5
texas0418 <simonshih@hotmail.com>
2019-02-25 11:50:04 -0600

    database mysql created

--- bamazon.sql
+++ bamazon.sql
@@ -14,31 +14,31 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Laptop", "Electronics", "599.99, 10");
+VALUES ("Laptop", "Electronics", "599.99", "10");
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Tablet", "Electronics", "299.99, 15");
+VALUES ("Tablet", "Electronics", "299.99", "15");
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Encore", "Eminem", "Rap");
+VALUES ("Smart Phone", "Electronics", "49.99", "20");
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-
+VALUES ("Japanese Ryoba Saw 180mm", "Tools", "34.80", "8");
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-
+VALUES ("Japanese Dozuki Hand Saw 6-in", "Tools", "24.85", "8");
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-
+VALUES ("Japanese Chisel Set", "Tools", "99.99", "5");
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Acoustic Guitar ", "Musical Instruments", "159.99", "6");
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ukulele", "Musical Instruments", "59.99", "4");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Drum Set", "Musical Instruments", "259.99", "2");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Digital Piano", "Musical Instruments", "399.99", "3");
