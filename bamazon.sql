8627031355e2c86a7dabd00bec49755bdecef824
texas0418 <simonshih@hotmail.com>
2019-02-25 11:29:14 -0600

    first commit

---
+++ bamazon.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NULL,
+  department_name VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Laptop", "Electronics", "599.99, 10");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tablet", "Electronics", "299.99, 15");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Encore", "Eminem", "Rap");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
